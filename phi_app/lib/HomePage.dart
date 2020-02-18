93d658a73714b3ec4a718bd9f63597cfa2696876
Basil Zuberi <basilzuberi@hotmail.com>
2020-02-18 17:29:00 -0500

    made Main run the HomePage file. HomePage.dart includes our "tabs" which can contain an events calendar and a tab for someones profile (can be changed)

---
+++ phi_app/lib/HomePage.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+class HomePage extends StatefulWidget{
+  //create a state for our homepage, since its reactive
+  @override
+  _HomePage createState() => new _HomePage ();
+
+}
+
+class _HomePage extends State <HomePage> with SingleTickerProviderStateMixin{
+
+
+  //tab controller so we can switch tabs
+  TabController _tabController;
+
+  @override
+  void initState(){
+    super.initState();
+    //we make the 'initial index' this specific page and have 2 tabs.
+    _tabController = new TabController(vsync: this,initialIndex: 1,length: 2);
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        //new app bar
+        title: new Text(" Stock App"),
+        elevation: 0.7,
+        //tab bar for pages
+        bottom: new TabBar(
+          //control with variable of tab controller called in initState
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            //the tabs (need 2 if defined 2)
+            new Tab(text:"Events"),
+            new Tab(text: "Profile"),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
